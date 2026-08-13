84dab43f97e73c333af5622a0bb3e1d433ce717e
sonic <alimoradishmash@gmail.com>
2026-08-13 23:32:56 +0330

    1.2.0 Ali

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,16 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Verification from "../pages/Verification";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/verification" element={<Verification />} />
     </Routes>
   );
 };
