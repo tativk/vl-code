04b2355938f277d4cebc77a284c38d756c5f4fd4
mobin <mobin123nezhad@gmail.com>
2026-08-23 03:03:26 +0330

    new 1.1.1

--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Learning/Hero";
 import PopularCourses from "../components/Learning/PopularCourses";
+import LearningPath from "../components/Learning/LearningPath";
 import "./Learning.css";
 
 const Learning = () => {
   return (
     <main className="Learning" dir="rtl" >
       <Hero />
+      <LearningPath />
       <PopularCourses />
       
     </main>
